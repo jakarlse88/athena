9310ae24fd39b93a5c6ecc45d379a41730c7c88a
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-09-09 14:53:45 +0200

    Initial Docker setup

---
+++ src/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Athena.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, world!");
+        }
+    }
+}
