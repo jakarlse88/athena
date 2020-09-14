0b072812b2405207c4d7c0c65b7569e77e4c466e
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-09-14 14:28:07 +0200

    Add test test

---
+++ test/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Athena.Test
+{
+    public class Test
+    {
+        [Fact]
+        public void TestTest()
+        {
+            var a = 1;
+            var b = 2;
+            var result = a + b;
+
+            Assert.Equal(3, result);
+        }
+    }
+}
