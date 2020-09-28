748e77cb7df816b1df952176d7a7373bbf7a48f2
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-09-28 18:00:37 +0200

    Revise routes

--- src/Controllers/TechniqueController.cs
+++ src/Controllers/TechniqueController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Athena.Models.Entities;
 using Athena.Models.ViewModels;
