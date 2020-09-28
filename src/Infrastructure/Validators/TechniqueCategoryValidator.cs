ca1c2710828e957f18a53f74aedc5e37bf0c6410
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-09-28 12:49:37 +0200

    Implement TechniqueCategory model validation

---
+++ src/Infrastructure/Validators/TechniqueCategoryValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using Athena.Models.ViewModels;
+using FluentValidation;
+
+namespace Athena.Infrastructure.Validators
+{
+    public class TechniqueCategoryValidator : AbstractValidator<TechniqueCategoryViewModel>
+    {
+        public TechniqueCategoryValidator()
+        {
+            RuleFor(model => model.Name)
+                .NotEmpty()
+                .MaximumLength(50)
+                .Matches(new Regex(@"^[a-zA-Z ]*$"));
+        }
+    }
+}
